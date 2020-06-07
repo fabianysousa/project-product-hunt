1973119c49d2eb703c7926754dadc47e7af139ee
Fabiany de Sousa <fabianydesousa@gmail.com>
2020-06-07 19:56:11 -0300

    adicionando banco api

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+
+// Iniciando o app
+const app = express();
+
+// Iniciando DB
+mongoose.connect('mongodb://localhost:27017/nodeapi', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+requireDir('./src/models');
+
+// Rotas
+app.use('/api', require('./src/routes'));
+
+app.listen(3001);
