4a425209afd6729db083853407134d085d17362d
Fabiany de Sousa <fabianydesousa@gmail.com>
2020-06-11 11:19:13 -0300

    finalizando api

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -4,14 +4,12 @@ const Product = mongoose.model('Product');
 
 module.exports = {
   async index(req, res) {
-    const products = await Product.find();
-
+    const { page = 1 } = req.query;
+    const products = await Product.paginate({}, { page, limit: 10 });
     return res.json(products);
   },
   async show(req, res) {
-    console.log(req);
     const product = await Product.findById(req.params.id);
-
     return res.json(product);
   },
   async store(req, res) {
